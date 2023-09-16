bb299eb39e85193900fa3737b284843161b6928b
jdheslop <j_heslop@hotmail.com>
2023-09-16 12:06:34 +0200

    Update to fulfill Learning Activity - C# Prep 2 including the stretch challenge.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine();
+
+        Console.Write("Please enter your grade percentage: ");
+        string gradePercentageInput = Console.ReadLine();
+        int gradePercentage = int.Parse(gradePercentageInput);
+
+        char inputLastDigit = gradePercentageInput[gradePercentageInput.Length - 1];
+        int gradeLastDigit = (int)Char.GetNumericValue(inputLastDigit);
+
+        string letterGrade = "";
+        string gradeSign = "";
+        string gradeResult = "";
+
+        if (gradeLastDigit >= 7)
+        {
+            gradeSign = "+";
+        }
+        else if (gradeLastDigit < 3)
+        {
+            gradeSign = "-";
+        }
+
+        if (gradePercentage >= 90)
+        {
+            if (gradeLastDigit < 3)
+            {
+                letterGrade = $"A{gradeSign}";
+            }
+            else
+            {
+                letterGrade = "A";
+            }
+        } 
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = $"B{gradeSign}";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = $"C{gradeSign}";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = $"D{gradeSign}";
+        }
+        else 
+        {
+            letterGrade = "F";
+        }
+
+        if (gradePercentage >= 70)
+        {
+            gradeResult = "Congratulations! You passed!";
+        }
+        else 
+        {
+            gradeResult = "You'll get it next time!";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+        Console.WriteLine(gradeResult);
+
+        Console.WriteLine();
     }
 }
