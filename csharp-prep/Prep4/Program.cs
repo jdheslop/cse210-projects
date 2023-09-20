fdaf8c2b5ffc1a90fbdcd368e653da1e38d380bc
jdheslop <j_heslop@hotmail.com>
2023-09-20 21:38:56 +0200

    Update to meet the core requirements of the Learning Activity - C# Prep 4 assignment.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine();
+
+        int numberInput = -1;
+        List<int> numbersList = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Enter numbers into the list
+        do
+        {
+            Console.Write("Enter number: ");
+            numberInput = int.Parse(Console.ReadLine());
+            if (numberInput != 0)
+            {
+                numbersList.Add(numberInput);
+            }
+        } while (numberInput != 0);
+
+        // Calculate the sum of the list
+        int sum = 0;
+        foreach (int number in numbersList)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Calculate the average of the list
+        float average = ((float)sum) / numbersList.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Find the largest number
+        int maxNumber = numbersList[0];
+        foreach (int number in numbersList)
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {maxNumber}");
+
+        Console.WriteLine();
     }
 }
