9923a88c84ec947139483f73bad6c3aa55dfbe83
jdheslop <j_heslop@hotmail.com>
2023-09-20 20:58:19 +0200

    Updated to complete the Learning Activity - C# Prep 3 (including the strech challenges) assignment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine();
+
+        string playAgain = "yes";
+        int guess;
+
+        
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+
+        while (playAgain == "yes")
+        {
+            int counter = 0;
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            do
+            {
+                counter ++;
+                Console.Write("What is your guess? ");
+                string guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+
+                if (magicNumber < guess)
+                {
+                    Console.WriteLine("The magic number is lower.");
+                }
+                else if (magicNumber > guess)
+                {
+                    Console.WriteLine("The magic number is higher.");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {counter} guesses.");
+                }
+            } while (magicNumber != guess);
+
+            Console.WriteLine();        
+            Console.Write("Would you like to play again? ");
+            playAgain = Console.ReadLine();
+            Console.WriteLine();
+        }
+        
+        Console.WriteLine("Thanks for playing!");
+        Console.WriteLine();
     }
 }
