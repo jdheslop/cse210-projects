bee2deb8835b22239f3fc780f2b90675e1e60966
jdheslop <j_heslop@hotmail.com>
2023-09-21 19:11:23 +0200

    Updated to fulfill the stretch challenges.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -36,15 +36,47 @@ class Program
         Console.WriteLine($"The average is: {average}");
 
         // Find the largest number
-        int maxNumber = numbersList[0];
+        int maxNumber = numbersList.Max();
+
+        /* int maxNumber = numbersList[0];
         foreach (int number in numbersList)
         {
             if (number > maxNumber)
             {
                 maxNumber = number;
             }
-        }
+        } */
+
         Console.WriteLine($"The largest number is: {maxNumber}");
+
+        // Find the smallest positive number
+        int smallestPositiveNumber = 999999999;
+        foreach (int number in numbersList)
+        {
+            if ((number > 0) && (number < smallestPositiveNumber))
+            {
+                smallestPositiveNumber = number;
+            }
+        }
+
+        // Display the smalles positive number result
+        if (smallestPositiveNumber != 999999999)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+        }
+        else
+        {
+            Console.WriteLine("None of the numbers are positive.");
+        }
+
+        // Sort the list
+        numbersList.Sort();
+
+        // Display the sorted list
+        foreach (int number in numbersList)
+        {
+            Console.WriteLine(number);
+        }
 
         Console.WriteLine();
     }
