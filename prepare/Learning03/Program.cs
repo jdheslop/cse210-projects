8875d961a16971fc01182e00e3aa8b626fa5a240
jdheslop <131174232+jdheslop@users.noreply.github.com>
2023-10-09 09:48:30 +0200

    Continued working on Encapsulation Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,17 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Fraction fraction1 = new Fraction();
+		Console.WriteLine($"Get numerator: {fraction1.GetNumerator()}");
+		Console.WriteLine($"Get denominator: {fraction1.GetDenominator()}");
+		Console.WriteLine(fraction1.GenerateFraction());
+		Console.WriteLine(fraction1.GenerateDecimel());
+	
         Fraction fraction2 = new Fraction(5);
-        Fraction fraction3 = new Fraction(3, 4);
-        Fraction fraction4 = new Fraction();
+		Console.WriteLine(fraction2.GenerateFraction());
+		Console.WriteLine(fraction2.GenerateDecimel());
 
+        Fraction fraction3 = new Fraction(3, 4);
+		Console.WriteLine(fraction3.GenerateFraction());
+		Console.WriteLine(fraction3.GenerateDecimel());
+
+        Fraction fraction4 = new Fraction();
         fraction4.SetNumerator(1);
         fraction4.SetDenominator(3);
-
-
-        Console.WriteLine(fraction1);
-        Console.WriteLine(fraction2);
-        Console.WriteLine(fraction3);
-
+		Console.WriteLine(fraction4.GenerateFraction());
+		Console.WriteLine(fraction4.GenerateDecimel());
     }
 }
+
